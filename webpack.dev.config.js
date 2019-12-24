10c8fb62f80c875781df3a0b628a7394e7680105
Martin Ortiz <martinortizpalacio@gmail.com>
2019-12-23 21:06:38 -0300

    solved webpack dev config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
